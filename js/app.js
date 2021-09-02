f4d3d2dcb63fca967920482855eba3d58a06b2eb
Shamim Ahmed <cse.shamimahmed@gmail.com>
2021-09-02 17:52:44 +0600

    search result update

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const searchBook = () => {
     const emptyText = document.getElementById('empty-text');
     if (searchText === '') {
         emptyText.innerHTML = `<h3 class="text-center fw-bold text-warning ">Please write something!!</h3>`;
+        clearScreen(searchText);
     }
     else {
         emptyText.textContent = '';
@@ -18,7 +19,7 @@ const resultFound = data => {
     const searchFound = document.getElementById('search-found');
     if (data.numFound === 0) {
         searchFound.innerHTML = `<h4 class="text-center text-warning ">No result found!!</h4>`;
-
+        clearScreen(data);
     }
     else {
 
@@ -30,21 +31,42 @@ const resultFound = data => {
 const displayResult = books => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    books.forEach(book => {
+    books.slice(0, 20).forEach(book => {
         console.log(book);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `<div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-thumbnail " alt="...">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid " alt="...">
             <div class="card-body">
             <h5 class="card-title">${book.title}</h5>
-                <p class="card-text "><span class="fw-bold">Author name: </span>${book.author_name}</p>
-                <p class="card-text "><span class="fw-bold">Publisher name: </span>${book.publisher}</p>
+                <p class="card-text"><span class="fw-bold">Author name: </span>${bookInfo(book.author_name)}</p>
+                <p class="card-text "><span class="fw-bold">Publisher name: </span>${bookInfo(book.publisher)}</p>
             </div>
             <div class="card-footer">
-                <small class="text-muted">First publish year is ${book.first_publish_year}</small>
+                <small class="text-muted">First publish year is ${bookInfo(book.first_publish_year)}</small>
             </div>
         </div>`;
         searchResult.appendChild(div);
     });
+}
+const bookInfo = data => {
+    if (data === undefined) {
+        return "unknown";
+    }
+    else {
+        return data[0];
+    }
+}
+const clearScreen = result => {
+    const searchResult = document.getElementById('search-result');
+    const searchFound = document.getElementById('search-found');
+    const emptyText = document.getElementById('empty-text');
+    if (result === '') {
+        searchResult.textContent = '';
+        searchFound.textContent = '';
+    }
+    else {
+        emptyText.textContent = '';
+        searchResult.textContent = '';
+    }
 }
